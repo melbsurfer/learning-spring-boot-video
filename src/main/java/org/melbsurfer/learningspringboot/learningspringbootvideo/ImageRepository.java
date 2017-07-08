cf6a1b596b32aeadce099cbe451a8ee8226f3fd5
melbsurfer <melbsurfer@gmail.com>
2017-07-08 17:20:32 -0400

    Added JPA repository for the Image object

---
+++ src/main/java/org/melbsurfer/learningspringboot/learningspringbootvideo/ImageRepository.java
@@ -0,0 +1,20 @@
+package org.melbsurfer.learningspringboot.learningspringbootvideo;
+
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+/**
+ *  JPA's PagingAndSortingRepository has two generic parameters that
+ *  it applies over.  In this case the Image, and the Id type
+ *  which is a Long.
+ */
+public interface ImageRepository extends PagingAndSortingRepository<Image, Long> {
+
+    /**
+     * A JPA finder that is meant to return an image.
+     *
+     * @param name
+     * @return
+     */
+    public Image findByName(String name);
+}
